8ad0e5792dd5d31f4959aa867f7e9729ddc49e8f
qinlei-sl <qinlei@sailing3D.cn>
2020-06-23 18:18:33 +0800

    623

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AccountService } from '@app/_services';
+import { AccountService, AlertService } from '@app/_services';
 import { first } from 'rxjs/operators';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
@@ -18,14 +18,22 @@ export class AddUserComponent implements OnInit {
   password;
   id;
   isAddMode: any;
+  flag;
   constructor(
     private accountService: AccountService,
     private route: ActivatedRoute,
     private formBuilder: FormBuilder,
+    private alertService: AlertService,
+    private router: Router,
     
     ) { }
 
   ngOnInit() {
+    // 获取页面是否为添加页面
+    this.id = this.route.snapshot.params['id']
+    //改变sava执行方法
+    this.flag = !this.id;
+    
     this.accountService.getAll()
             .pipe(first())
             .subscribe(users => this.users = users);
@@ -39,17 +47,47 @@ export class AddUserComponent implements OnInit {
       lastName: ['', Validators.required],
       username: ['', Validators.required],
       password: ['', passwordValidators]
-  });
-  }
-  change(){
-    console.log(this.f.firstName)
+    });
     this.accountService.getById(this.id)
-    .pipe(first())
-    .subscribe(x => {
+      .pipe(first())
+      .subscribe(x => {
         this.f.firstName.setValue(x.firstName);
         this.f.lastName.setValue(x.lastName);
         this.f.username.setValue(x.username);
     });
+  }
+  onsubmit() {
+    if (this.flag) {
+      this.change()
+    } else {
+      this.create()
+    }
+  }
+  // 改当前用户的数据
+  private change() {
+    this.accountService.update(this.id, this.form.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.alertService.success('Update successful', { keepAfterRouteChange: true });
+          this.router.navigate(['..', { relativeTo: this.route }]);
+        },
+        error => {
+          this.alertService.error(error);
+        });
+  }
+  // 创建新用户
+  private create() {
+    this.accountService.register(this.form.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.alertService.success('Update successful', { keepAfterRouteChange: true });
+          this.router.navigate(['..', { relativeTo: this.route }]);
+        },
+        error => {
+          this.alertService.error(error);
+        });
   }
   get f() { return this.form.controls; }
 
